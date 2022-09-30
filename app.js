ecffa2233b8673d27164df5ac97111ddad043384
Adimchi <oraegbunamadimchi@gmail.com>
2022-09-30 12:31:47 +0100

    selected elements for DOM manipulation

---
+++ app.js
@@ -0,0 +1,5 @@
+const slider = document.getElementById('discount-slider')
+const pageViews = document.getElementById('view')
+const price = document.getElementById('price')
+const checkbox = document.getElementById('checkbox')
+
